6daf5a85271d82a09d5f48d740a6398b3e899cb7
Kai Naschinski <kai@Kais-MacBook-Pro.local>
2016-11-18 11:55:43 +0100

    refactor some code, minor changes to neck-percentages

--- kokoBuilder.js
+++ kokoBuilder.js
@@ -1,5 +1,5 @@
-var exeggutor = "Lorem";
 exports.getKoko = function(){
+  var exeggutor;
   var rng = Math.random();
   if(rng > 0.9){
     exeggutor = ":kokomohr:\n";
@@ -8,38 +8,36 @@ exports.getKoko = function(){
     exeggutor = ":kokohead:\n";
   }
   rng = Math.floor((Math.random() * 5) + 1);
-  for (var i = 0; i < rng; i++) {
-    exeggutor += getKokoBody();
-  }
-
+  exeggutor += getKokoBody(rng);
   exeggutor += ":kokolegs:";
   return exeggutor;
 };
 
-function getKokoBody(){
-  console.log("hadijwoiawdjoi");
-  var exegBody;
-  var rng = Math.random();
-  if(rng < 0.66){
-    exegBody = ":kokoneck:\n";
+function getKokoBody(height){
+  var exeggBody = "";
+  for(var i = 0; i < height; i++){
+    var rng = Math.random();
+    if(rng < 0.66){
+      exeggBody += ":kokoneck:\n";
+    }
+    else if(rng < 0.77){
+      exeggBody += ":kokokoala:\n";
+    }
+    else if (rng < 0.85) {
+      exeggBody += ":kokocloud:\n";
+    }
+    else if (rng < 0.9) {
+      exeggBody += ":kokotwist:\n";
+    }
+    else if (rng < 0.945) {
+      exeggBody += ":kokousa:\n";
+    }
+    else if (rng < 0.98) {
+      exeggBody += ":kokobear:\n";
+    }
+    else {
+      exeggBody += (":kokoportal:\n");
+    }
   }
-  else if(rng < 0.77){
-    exegBody = ":kokokoala:\n";
-  }
-  else if (rng < 0.85) {
-    exegBody = ":kokocloud:\n";
-  }
-  else if (rng < 0.9) {
-    exegBody = ":kokotwist:\n";
-  }
-  else if (rng < 0.95) {
-    exegBody = ":kokousa:\n";
-  }
-  else if (rng < 0.99) {
-    exegBody = ":kokobear:\n";
-  }
-  else {
-    exegBody = (":kokoportal:\n");
-  }
-  return exegBody;
+  return exeggBody;
 }
