a55ca34ae8bcd5b1b493d51d57b243bc54c2d469
Jakob Schrettenbrunner <dev@schrej.net>
2016-11-19 02:42:02 +0100

    add keyword to 60 tons remove dumb keyword

--- answer.js
+++ answer.js
@@ -7,8 +7,8 @@ function add(keys, message) {
   })
 }
 
-add(['verantwortung', 'ver'], 'Ich hab doch die Verantwortung über 20 Leute!');
-add(['verdichtet'], 'Da soll ich jetzt 60 Tonnen drauf abstellen?!');
+add(['verantwortung'], 'Ich hab doch die Verantwortung über 20 Leute!');
+add(['verdichtet', 'kranplatz'], 'Da soll ich jetzt 60 Tonnen drauf abstellen?!');
 add(['norwegen'], 'Darum sind die auch nicht in der EU, weil die am Leben vorbeilaufen ... diese Spinnerbande!');
 add(['zu hause'], 'Sollen wir nach Hause fahr\'n oder wat?');
 add(['kann nichts'], 'Junge, Junge, Junge, Junge, Junge, Junge, Junge, Junge, Junge, Junge!');
