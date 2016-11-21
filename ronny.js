35a0a3130aa33aafeb0fcc98f5e002ec4cd90e84
Jakob Schrettenbrunner <dev@schrej.net>
2016-11-21 12:40:34 +0100

    add .eslintrc
    adapt to defined codestyle

--- ronny.js
+++ ronny.js
@@ -1,37 +1,41 @@
-var SlackBot = require('slackbots');
-var kokoBuilder = require('./kokoBuilder.js');
-var dict = require('./answer.js');
+const SlackBot = require('slackbots');
+const dict = require('./answer.js');
 
 // create a bot
-var bot = new SlackBot({
-    token: 'xoxb-99562714261-fC6JuSV19uhowciMrAMh8ttw',
-    name: 'Ronnie Schäfer'
+const bot = new SlackBot({
+  token: 'xoxb-99562714261-fC6JuSV19uhowciMrAMh8ttw',
+  name: 'Ronnie Schäfer'
 });
 
-var params = {as_user:true};
+const params = {
+  as_user: true
+};
 
-bot.on('start', function (data) {
-	bot.postMessageToGroup('dev', 'BOT RUNNING', params);
+bot.on('start', function(data) {
+  bot.postMessageToGroup('dev', 'BOT RUNNING', params);
 });
 
 bot.on('message', function(data) {
 
-	console.log("=============================================");
-	console.log(data);
+  console.log('=============================================');
+  console.log(data);
 
-	switch (data.type) {
-		case 'message' : {
-			toChannel = data.channel;
-			if(toChannel && data.subtype !== 'bot_message' && data.user !== 'U2XGJM07P') {
-				msg = data.text.toLowerCase();
-        dict.forEach(d => {
-          if (d.keys.some(k => ~msg.indexOf(k))) {
-            var message = d.message
-            if (typeof message === 'function') message = message()
-            bot.postMessage(toChannel, message, params);
-          }
-        });
-			}
-		}
-	}
+  switch (data.type) {
+    case 'message':
+      {
+        var toChannel = data.channel;
+        if (toChannel && data.subtype !== 'bot_message'
+          && data.user !== 'U2XGJM07P') {
+          var msg = data.text.toLowerCase();
+          dict.forEach(d => {
+            if (d.keys.some(k => ~msg.indexOf(k))) {
+              var message = d.message;
+              if (typeof message === 'function')
+                message = message();
+              bot.postMessage(toChannel, message, params);
+            }
+          });
+        }
+      }
+  }
 });
